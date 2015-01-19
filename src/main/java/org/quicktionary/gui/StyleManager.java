58df137ceaddf19ca44eb613f41b3a17808495dd
Aleksi <aleksi.salmela@helsinki.fi>
2015-01-19 22:16:45 +0200

    Add support for gradients in style manager.

--- src/main/java/org/quicktionary/gui/StyleManager.java
+++ src/main/java/org/quicktionary/gui/StyleManager.java
@@ -17,29 +17,53 @@
 package org.quicktionary.gui;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Paint;
+import java.awt.GradientPaint;
+import java.awt.geom.Point2D;
+
+import java.lang.IllegalStateException;
 
 public class StyleManager {
-	static HashMap<String, Color> colors;
+	static HashMap<String, Object> styles;
+	static ArrayList<StyleListener> listeners;
 
 	public StyleManager() {
-		colors = new HashMap<String, Color>();
+		styles = new HashMap<String, Object>();
+		listeners = new ArrayList<StyleListener>();
 	}
 
 	private void useDarkStyle() {
-		colors.put("header-bg",            new Color(0x3c3b38));
-		colors.put("header-button-top",    new Color(0x3e3d38));
-		colors.put("header-button-bottom", new Color(0x3a3836));
-		colors.put("header-button-border", new Color(0x313330));
-		colors.put("header-color",         new Color(0xe1d8c9));
+		GradientPaint button;
+		button = new GradientPaint(0, 0, new Color(0x3e3d38),
+		                           0, 1, new Color(0x3a3836), true);
+		styles.put("header-bg",            new Color(0x3c3b38));
+		styles.put("header-button-bg",     button);
+		styles.put("header-button-active-bg", new Color(0x363632));
+		styles.put("header-button-border", new Color(0x313330));
+		styles.put("header-color",         new Color(0xe1d8c9));
 	}
 
 	private void useLightStyle() {
-		colors.put("header-bg",            new Color(0xf3f3f3));
-		colors.put("header-button-top",    new Color(0xf6f6f6));
-		colors.put("header-button-bottom", new Color(0xdcdcdc));
-		colors.put("header-button-border", new Color(0xb2b6b2));
-		colors.put("header-color",         new Color(0x303638));
+		GradientPaint button, buttonActive;
+		button = new GradientPaint(0, 0, new Color(0xf6f6f6),
+		                           0, 1, new Color(0xdcdcdc), true);
+		buttonActive = new GradientPaint(0, 0, new Color(0xd6d6d6),
+		                                 0, 1, new Color(0xe0e0e0), true);
+
+		styles.put("header-bg",            new Color(0xf3f3f3));
+		styles.put("header-button-bg",     button);
+		styles.put("header-button-active-bg", buttonActive);
+		styles.put("header-button-border", new Color(0xb2b6b2));
+		styles.put("header-color",         new Color(0x303638));
+	}
+
+	private void refreshGui() {
+		for(StyleListener listener : listeners) {
+			listener.styleChanged();
+		}
 	}
 
 	public void changeStyle(String name) {
@@ -48,11 +72,50 @@ public class StyleManager {
 		} else if(name.equals("light")) {
 			useLightStyle();
 		} else {
-			colors.clear();
+			styles.clear();
 		}
+
+		refreshGui();
+	}
+
+	public static void setSource(Graphics2D graphics2d,
+	                             Paint source,
+	                             int width, int height) {
+		if(source instanceof GradientPaint) {
+			Point2D.Float p1, p2;
+			GradientPaint gradient = (GradientPaint)source;
+
+			/* stretch the gradient to the size of the component */
+			p1 = (Point2D.Float)gradient.getPoint1();
+			p2 = (Point2D.Float)gradient.getPoint2();
+			p1.x *= width;
+			p1.y *= height;
+			p2.x *= width;
+			p2.y *= height;
+			source = new GradientPaint(p1, gradient.getColor1(),
+			                           p2, gradient.getColor2());
+		}
+		graphics2d.setPaint(source);
+	}
+
+	static Object getStyle(String styleId) {
+		return styles.get(styleId);
 	}
 
 	static Color getColor(String colorId) {
-		return colors.get(colorId);
+		Object color;
+		color = getStyle(colorId);
+		if(!(color instanceof Color)) {
+			throw new IllegalStateException("Style must have Color type.");
+		}
+		return (Color)color;
+	}
+
+	static void setStyleListener(StyleListener listener) {
+		listeners.add(listener);
+	}
+
+	public interface StyleListener {
+		void styleChanged();
 	}
 }
