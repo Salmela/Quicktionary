d979c9560e714f5b1754ed10a6d70da88459c408
Aleksi <aleksi.salmela@helsinki.fi>
2015-01-22 14:25:12 +0200

    Make the XMLParser almost complete.

--- src/main/java/org/quicktionary/backend/parsers/XMLParser.java
+++ src/main/java/org/quicktionary/backend/parsers/XMLParser.java
@@ -21,6 +21,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.lang.StringBuilder;
+
 /**
  * This class implements offline xml parser. It doesn't
  * allow custom markup defined inside file. The code
@@ -35,16 +40,40 @@ import java.io.IOException;
  * @author alesalme
  */
 public class XMLParser {
-	private BufferedReader reader;
-	private byte currentChar;
+	private BufferedReader          reader;
+	private ArrayList<Integer>      parentNodes;
+	private HashMap<CharSequence, Integer> tagNames;
 
-	public XMLParser() {
-	}
+	/**
+	 * The current char must be at the start of next node
+	 * after any method.
+	 */
+	private byte currentChar;
+	private boolean parsingError;
+	private boolean preserveWhitespaces;
+
+	private NodeType      nodeType;
+	private TagType       tagType;
+	private StringBuilder tagName;
+	private int tagNameId;
 
 	public enum NodeType {
-		ELEMENT_NODE,
-		COMMENT_NODE,
-		TEXT_NODE
+		ELEMENT,
+		COMMENT,
+		TEXT
+	}
+
+	private enum TagType {
+		START,
+		END,
+		EMPTY
+	}
+
+	public XMLParser() {
+		parentNodes = new ArrayList<Integer>(32);
+		tagNames = new HashMap<CharSequence, Integer>();
+		tagName  = new StringBuilder(256);
+		reader   = null;
 	}
 
 	/**
@@ -56,6 +85,7 @@ public class XMLParser {
 	public void parseFile(File file) throws IOException {
 		reader = new BufferedReader(new FileReader(file));
 		currentChar = 0;
+		preserveWhitespaces = true;
 		/* initialize the currentChar */
 		getNext();
 		/* read the declaration */
@@ -63,31 +93,133 @@ public class XMLParser {
 	}
 
 	/* parsing interface */
+	public int getTagNameId(String tagName) {
+		return getTagNameId(tagName);
+	}
+
+	public int getTagNameId(CharSequence tagName) {
+		int id;
+
+		if(tagNames.containsKey(tagName)) {
+			id = tagNames.size();
+			tagNames.put(tagName, id);
+		} else {
+			id = tagNames.get(tagName);
+		}
+		return id;
+	}
+
 	public boolean getRoot() {
+		/* check that we are at the start of the file */
+		if(parentNodes.size() != 0) {
+				appendLog("The reader has already readed the root node.");
+				return false;
+		}
+
+		return parseNode();
 	}
-	public boolean getElement(String tagName) {
-	}
-	public boolean getAttribute(String attributeName) {
-	}
-	public enum NodeType getNodeType() {
-	}
+
 	/**
-	 * Get the element's tag name. If current node isn't element, then
+	 * Go to the first element with the tagNameId.
+	 */
+	public boolean findElement(int tagNameId) {
+		while(parseNode()) {
+			if(this.tagNameId == tagNameId) {
+				break;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Go to the first element with the tagName.
+	 */
+	public boolean findElement(String tagName) {
+		return findElement(getTagNameId(tagName));
+	}
+
+	/**
+	 * Go to the end of the parent node.
+	 */
+	public boolean getParent() {
+		int parentIndex = parentNodes.size();
+
+		while(parseNode()) {
+			if(parentNodes.size() == parentIndex - 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean getFirstChild() {
+		return false;
+	}
+
+	public boolean getNextSibling() {
+		int parentIndex = parentNodes.size();
+		while(parseNode()) {
+			if(parentNodes.size() == parentIndex) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get the value of specific attribute at current element.
+	 */
+	public String getAttribute(String attributeName) {
+		return null;
+	}
+
+	public NodeType getNodeType() {
+		return nodeType;
+	}
+
+	/**
+	 * Get the tag name of current element. If current node isn't element, then
 	 * return null.
 	 */
 	public String getElementName() {
+		return tagName.toString();
 	}
 	/**
-	 * Get next attribute. If any attributes haven't been
+	 * Get next attribute at the current element. If any attributes haven't been
 	 * parsed then the first one is given.
 	 */
 	public String getNextAttribute() {
-	}
-	public boolean getFirstChild() {
-	}
-	public boolean getNextSibling() {
+		return null;
 	}
 
+	/**
+	 * Change the default of ignoring whitespaces.
+	 */
+	public void setWhitespacePreserving(boolean preserveWhitespaces) {
+		this.preserveWhitespaces = preserveWhitespaces;
+	}
+
+	/**
+	 * This method prints parsing errors to log.
+	 */
+	private void appendLog(String message) {
+		System.out.println("Parsing error occured: " + message);
+		String trace = Arrays.toString(Thread.currentThread().getStackTrace());
+		System.out.println(trace);
+		parsingError = true;
+	}
+
+	private void appendLog(Exception exception) {
+		System.out.println("Parsing error occured: " + exception.getMessage());
+		exception.printStackTrace();
+		parsingError = true;
+	}
+
+	/**
+	 * Check if letter is in ascii alphabet.
+	 * @param letter The character that we want to check
+	 * @return True, if the letter was in ascii alphabet
+	 */
 	private boolean isAlphabet(byte letter) {
 		if(letter >= 'a' && letter <= 'z') {
 			return true;
@@ -97,6 +229,13 @@ public class XMLParser {
 		}
 		return false;
 	}
+
+	/**
+	 * Check if letter is whitespace. The method consideres
+	 * space, newline, tab, and carriage return as whitespaces.
+	 * @param letter The character that we want to check
+	 * @return True, if the letter was ascii whitespace
+	 */
 	private boolean isWhitespace(byte letter) {
 		if(letter == ' ' || letter == '\t' || letter == '\n' ||
 		   letter == '\r') {
@@ -105,14 +244,23 @@ public class XMLParser {
 		return false;
 	}
 
+	/**
+	 * Read next character from file.
+	 * @return The readed character
+	 */
 	private byte getNext() {
 		try {
 			currentChar = (byte)reader.read();
 		} catch(Exception e) {
+			currentChar = 0;
 		}
 		return currentChar;
 	}
 
+	/**
+	 * Skip over all following whitespaces. The method throws exception if
+	 * the current byte isn't whitespace and the atLeastOne parameter is set.
+	 */
 	private void skipWhitespaces(boolean atLeastOne) {
 		/* check that there is whitespace */
 		if(!isWhitespace(currentChar)) {
@@ -134,7 +282,7 @@ public class XMLParser {
 
 	/**
 	 * Parse a attribute of the element. We must be at the start of attribute.
-	 * some XML like languages allows attribute values to be unquoted
+	 * Note: Some XML-like languages allows attribute values to be unquoted.
 	 */
 	private boolean parseAttribute() {
 		char quoteChar;
@@ -158,7 +306,8 @@ public class XMLParser {
 		skipWhitespaces(false);
 
 		if(currentChar != '\"' && currentChar != '\'') {
-			throw new Error("Attribute's value must be enclosed inside quotes.");
+			appendLog("Attribute's value must be enclosed inside quotes.");
+			return false;
 		} else {
 			quoteChar = (char)currentChar;
 			getNext();
@@ -172,34 +321,51 @@ public class XMLParser {
 
 		if(currentChar != quoteChar) {
 			/* some xml like languages allows attribute values to be unquoted */
-			throw new Error("Expected " + quoteChar +" quote, but was " + currentChar + ".");
+			appendLog("Expected " + quoteChar +" quote, but was " + currentChar + ".");
+			return false;
 		} else {
-			/* consume the quote and the following whitespaces */
+			/* consume the quote */
 			getNext();
 		}
 		return true;
 	}
 
+	/**
+	 * Parse one tag from reader.
+	 * Reader is expected to be start of the tag.
+	 */
 	private void parseTag() {
-		boolean endTag;
-		byte letter;
+		tagType = TagType.START;
 
-		endTag = false;
+		/* reset the length */
+		tagName.setLength(0);
 
 		/* read the first byte of the tag */
-		readChar('<', "Element must start with less-than sign.");
+		if(currentChar != '<') {
+			appendLog("Element must start with less-than sign.");
+			return;
+		}
 
 		/* check if the element is end tag */
 		if(currentChar == '/') {
-			endTag = true;
+			tagType = TagType.END;
 			getNext();
+
+		/* check if the element is comment */
+		} else if(currentChar == '-') {
+			/*TODO implement this */
+			parsingError = true;
+			nodeType = NodeType.COMMENT;
+			return;
 		}
+		nodeType = NodeType.ELEMENT;
 
 		/* read the name of element */
 		while(isAlphabet(currentChar)) {
 			/* push this char to element name */
-			getNext();
+			tagName.append(getNext());
 		}
+		tagNameId = getTagNameId(tagName);
 
 		skipWhitespaces(true);
 
@@ -210,19 +376,83 @@ public class XMLParser {
 
 		/* check if this is inline element */
 		if(currentChar == '/') {
-			if(endTag) {
-				throw new Error("The tag cannot be empty tag and end tag at the same time.");
+			if(tagType == TagType.END) {
+				appendLog("The tag cannot be empty tag and end tag at the same time.");
 			}
+			tagType = TagType.EMPTY;
 			getNext();
 		}
 
 		readChar('>', "Element must end with more-than sign.");
 	}
 
+	private boolean parseTextContent() {
+		nodeType = NodeType.TEXT;
+		while(currentChar != '<') {
+			getNext();
+		}
+		return true;
+	}
+
+	private boolean parseRootNode() {
+		while(true) {
+			if(!parseNode()) {
+				return false;
+			}
+
+			if(nodeType == NodeType.TEXT) {
+				appendLog("Text content is not allowed outside of root node.");
+				return false;
+			} else if(nodeType == NodeType.ELEMENT) {
+				break;
+			}
+		}
+		return true;
+	}
+
+	private boolean parseNode() {
+		parsingError = false;
+
+		if(preserveWhitespaces) {
+			skipWhitespaces(false);
+		}
+
+		try {
+			if(parentNodes.size() != 0) {
+				parseRootNode();
+			} else if(currentChar == '<') {
+				parseTag();
+			} else {
+				parseTextContent();
+			}
+		} catch(Exception exception) {
+			appendLog(exception);
+			return false;
+		}
+
+		/* update the parent node array */
+		if(nodeType == NodeType.ELEMENT) {
+			if(tagType == TagType.START) {
+				parentNodes.add(tagNameId);
+
+			} else if(tagType == TagType.END) {
+				int index;
+				index = parentNodes.size() - 1;
+				if(parentNodes.get(index) == tagNameId) {
+					appendLog("Mismatching tags.");
+				} else {
+					parentNodes.remove(index);
+				}
+			}
+		}
+
+		return !parsingError;
+	}
+
 	private void parseXMLDeclaration() {
 		skipWhitespaces(false);
 
-		readChar('<', "File must start with xml declaration");
+		readChar('<', "File must start with xml declaration.");
 		readChar('?', "File must start with xml declaration.");
 		readChar('x', "File must start with xml declaration.");
 		readChar('m', "File must start with xml declaration.");
