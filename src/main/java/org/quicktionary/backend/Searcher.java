1c2fc7c51af0b5a1dcb740bd5a6c4cc69c5f371f
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-01-29 00:27:07 +0200

    Implement the hasCompleted method and add comments at the Searcher class.

--- src/main/java/org/quicktionary/backend/Searcher.java
+++ src/main/java/org/quicktionary/backend/Searcher.java
@@ -16,15 +16,15 @@
  */
 package org.quicktionary.backend;
 
-import java.util.Map;
-
 /**
- * The search queries send by the interface are parsed and processed in this class.
+ * The Searcher class parses the search queries from gui and
+ * sends results back to gui.
  */
 public class Searcher {
 	private Quicktionary dictionary;
 	private WordDatabase database;
 	private int processedEntries;
+	private boolean searchRunning;
 
 	private SearchResultListener resultListener;
 
@@ -32,28 +32,63 @@ public class Searcher {
 		this.dictionary = dictionary;
 		this.database = database;
 		this.processedEntries = 0;
+		this.searchRunning = false;
 	}
 
+	/**
+	 * Search words from WordDatabase that are similar
+	 * to search query. This method is called for all
+	 * search events coming from gui.
+	 */
 	public void search(String query) {
 		parseSearchQuery(query);
 
 		processedEntries = 0;
-		database.requestResults(query);
+		searchRunning = true;
 		resultListener.resetSearchResults();
+
+		/*TODO: move to another thread */
+		database.requestResults(query);
 	}
 
+	/**
+	 * The method returns true if all search results are
+	 * given to the SearchResultListener.
+	 *
+	 * This is a helper method for tests.
+	 * @return true if the search is complete
+	 */
+	public boolean hasCompleted() {
+		return this.searchRunning == false;
+	}
+
+	/**
+	 * Set the SearchResultListener. All future search results
+	 * are given to the listener.
+	 */
 	public void setResultListener(SearchResultListener listener) {
 		resultListener = listener;
 	}
 
-	public void requestSearchResults(int offset, int count) {
+	/**
+	 * Fetch the words from WordDatabase that fit the search term
+	 * and send them to SearchResultListener. This method also
+	 * will possibly reorder the search results.
+	 */
+	public boolean requestSearchResults(int offset, int count) {
 		WordDatabase.WordEntry[] entries;
 		int i;
 
-		if(resultListener == null) return;
+		if(resultListener == null) {
+			searchRunning = false;
+			return false;
+		}
 
 		count = offset + count - processedEntries;
-		if(count <= 0) return;
+		if(count <= 0) {
+			searchRunning = false;
+			return false;
+		}
 
 		entries = new WordDatabase.WordEntry[count];
 		count = database.fetchResults(entries, count);
@@ -62,8 +97,15 @@ public class Searcher {
 			resultListener.appendSearchResult(new SearchItem(entries[i].getWord(), "Test", entries[i]));
 		}
 		processedEntries += count;
+
+		searchRunning = false;
+		return true;
 	}
 
+	/**
+	 * Split the search query to parts. Currently the
+	 * method is just nop.
+	 */
 	private String parseSearchQuery(String query) {
 		return query;
 	}
