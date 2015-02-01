4ac80fdb413554a7b3a5b1ff84c7371340fecc6c
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-02-02 01:15:49 +0200

    Add support for WikiMarkup testing at gui.Test .

--- src/main/java/org/quicktionary/gui/Test.java
+++ src/main/java/org/quicktionary/gui/Test.java
@@ -4,32 +4,51 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import org.quicktionary.backend.parsers.XMLParser;
+import org.quicktionary.backend.parsers.WikiMarkup;
 
 /*
  * this file is for debugging the test cases.
  */
 public class Test {
-	private XMLParser parser;
+	private XMLParser  parserXML;
+	private WikiMarkup parserWiki;
 
 	String xmlDecl = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
 
 	public Test() {
-		parser = new XMLParser();
+		parserXML = new XMLParser();
+		parserWiki = new WikiMarkup();
 
-		emptyDocumentIsValid();
+		xmlParserTest();
+		wikiParserTest();
 	}
 
-	public void parseString(String fileText) {
+	public static void main(String[] args) {
+		new Test();
+	}
+
+	public void parseString(String xmlText) {
 		try {
-			parser.parseFile(new StringReader(fileText));
+			parserXML.parseFile(new StringReader(xmlText));
 		} catch(Exception e) {
 		}
 	}
 
-	public void emptyDocumentIsValid() {
-		parseString(xmlDecl + "<test><!-- this is comment -></test>");
+	public void parse(String wikiText) {
+		try {
+			parserWiki.parse(new StringReader(wikiText));
+		} catch(Exception e) {
+		}
+	}
 
-		parser.findElement("");
-		System.out.println(!parser.parsingErrorHappened());
+	public void xmlParserTest() {
+		parseString(xmlDecl + "<test><!-- this is comment --></test>");
+
+		parserXML.findElement("");
+		System.out.println(!parserXML.parsingErrorHappened());
+	}
+
+	public void wikiParserTest() {
+		parse("== t'''est''' == \nhello");
 	}
 }
