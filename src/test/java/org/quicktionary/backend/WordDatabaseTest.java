cffae7740c3733f8658d772563bf1d8840b2a01e
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-02-05 16:28:57 +0200

    Create a stub code for WordDatabase test suite.

---
+++ src/test/java/org/quicktionary/backend/WordDatabaseTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.quicktionary.backend;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alesalme
+ */
+public class WordDatabaseTest {
+	
+	public WordDatabaseTest() {
+	}
+
+	//@Test
+	//public void hello() {
+	//}
+}
