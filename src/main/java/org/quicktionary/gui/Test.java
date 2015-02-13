cfa782e2b2ff8ae6ac31551912622b36366243c2
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-02-13 20:21:34 +0200

    More tests for WikiMarkup.

--- src/main/java/org/quicktionary/gui/Test.java
+++ src/main/java/org/quicktionary/gui/Test.java
@@ -23,7 +23,16 @@ public class Test {
 
 		xmlParserTest();
 
+		whitespaceBefore();
+		whitespaceAfter();
+		whitespaceMiddle();
+
+		unendingTextStyle();
+		multiLineTemplate();
+		invalidMultiLineTemplate();
+
 		headerAndParagraph();
+		headerWithEmAndParagraph();
 		twoParagraphs();
 
 		templateWithPartialStrongMarkup();
@@ -71,25 +80,136 @@ public class Test {
 		System.out.println(!parserXML.parsingErrorHappened());
 	}
 	public void wikiParserTest() {
-		TextFragment wanted, node;
-		wanted = parserWiki.new TextFragment(TextFragment.ROOT_TYPE);
-		wanted.appendChild(parserWiki.new TextFragment(TextFragment.HEADER_TYPE));
-		wanted.appendChild(parserWiki.new TextFragment(TextFragment.PARAGRAPH_TYPE));
-
-		fragment = parse("== t'''est''' == \nhello");
-		System.out.println("Result: " + fragment.equals(wanted));
 	}
 
 	/* WikiMarkup */
-	public void headerAndParagraph() {
-		TextFragment wanted, node;
-		wanted = parserWiki.new TextFragment(TextFragment.ROOT_TYPE);
-		wanted.appendChild(parserWiki.new TextFragment(TextFragment.HEADER_TYPE));
-		wanted.appendChild(parserWiki.new TextFragment(TextFragment.PARAGRAPH_TYPE));
+	private TextFragment newNode(TextFragment parent, String text, int type, String parameter) {
+		TextFragment node;
+		if(parent == null) {
+			node = parserWiki.new TextFragment(type, parameter);
+		} else {
+			node = parent.appendChild(parserWiki.new TextFragment(type, parameter));
+		}
+		if(text != null) {
+			TextFragment textNode;
+			textNode = parserWiki.new TextFragment(TextFragment.PLAIN_TYPE);
+			node.appendChild(textNode);
+			textNode.setContent(text);
+		}
+		return node;
+	}
+	private TextFragment newNode(TextFragment parent, int type, String parameter) {
+		return newNode(parent, null, type, parameter);
+	}
+	private TextFragment newNode(TextFragment parent, int type) {
+		return newNode(parent, null, type, null);
+	}
+	private TextFragment newNode(TextFragment parent, String text, int type) {
+		return newNode(parent, text, type, null);
+	}
+	private void addText(TextFragment parent, String text) {
+		TextFragment textNode;
 
-		fragment = parse("== t'''est''' == \nhello");
+		if(parent == null) {
+			throw new Error("Parent must be set");
+		}
+
+		textNode = parserWiki.new TextFragment(TextFragment.PLAIN_TYPE);
+		parent.appendChild(textNode);
+		textNode.setContent(text);
+	}
+
+	public void headerAndParagraph() {
+		TextFragment wanted, header;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "test", TextFragment.HEADER_TYPE);
+		newNode(wanted, "hello", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("== test == \nhello");
 		System.out.println("Result: " + fragment.equals(wanted));
 	}
+	public void headerWithEmAndParagraph() {
+		TextFragment wanted, header;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		header = newNode(wanted, TextFragment.HEADER_TYPE);
+		newNode(wanted, "hello", TextFragment.PARAGRAPH_TYPE);
+
+		addText(header, "t");
+		newNode(header, "est", TextFragment.EM_TYPE);
+		addText(header, "er");
+
+		fragment = parse("== t'''est'''er == \nhello");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+
+	public void whitespaceBefore() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "test ");
+		newNode(paragraph, "cool", TextFragment.EM_TYPE);
+
+		fragment = parse("test ''' cool'''");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+	public void whitespaceAfter() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		newNode(paragraph, "cool ", TextFragment.EM_TYPE);
+		addText(paragraph, "test");
+
+		fragment = parse("'''cool ''' test");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+	public void whitespaceMiddle() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		newNode(paragraph, "cool ", TextFragment.EM_TYPE);
+		addText(paragraph, "test");
+
+		fragment = parse("'''cool ''' '' test''");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+
+	public void unendingTextStyle() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "This is ");
+		newNode(paragraph, "so cool", TextFragment.EM_TYPE);
+
+		fragment = parse("This is '''so cool");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+
+	public void multiLineTemplate() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "This is ");
+		newNode(paragraph, "smallcaps\n|so cool\n\n", TextFragment.EM_TYPE);
+
+		fragment = parse("This is {{smallcaps\n|so cool\n\n}}");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+	public void invalidMultiLineTemplate() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		newNode(wanted, "This is {{smallcaps a|so cool", TextFragment.PARAGRAPH_TYPE);
+		newNode(wanted, "}}", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("This is {{smallcaps\na|so cool\n\n}}");
+		System.out.println("Result: " + fragment.equals(wanted));
+	}
+
 	public void templateWithPartialStrongMarkup() {
 		fragment = parse("helloha{{lgh|uh'''gr}}eugh");
 	}
