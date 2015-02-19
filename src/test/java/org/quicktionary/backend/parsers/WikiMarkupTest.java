59036073748f63127dd19a61c9a74ae8077c2965
Aleksi Salmela <aleksi.salmela@helsinki.fi>
2015-02-19 15:35:10 +0200

    Add the WikiMarkup tests to the test suite.

--- src/test/java/org/quicktionary/backend/parsers/WikiMarkupTest.java
+++ src/test/java/org/quicktionary/backend/parsers/WikiMarkupTest.java
@@ -21,79 +21,571 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Assume;
 import org.junit.rules.Timeout;
+import static org.junit.Assert.*;
 
 import java.io.StringReader;
-import org.junit.Assume;
 
-import org.quicktionary.backend.parsers.WikiMarkup;
+import static org.quicktionary.backend.parsers.WikiMarkup.TextFragment;
 
 public class WikiMarkupTest {
-	private WikiMarkup parser;
+	private WikiMarkup parserWiki;
+	private TextFragment fragment;
 
 	@Rule
 	public Timeout globalTimeout = new Timeout(100);
 
 	public WikiMarkupTest() {
-		parser = new WikiMarkup();
+		parserWiki = new WikiMarkup();
 	}
 
-	private boolean parse(String markupString) {
+	private TextFragment parse(String markupString) {
 		try {
-			return parser.parse(new StringReader(markupString));
+			parserWiki.parse(new StringReader(markupString));
+			return parserWiki.getRoot();
 		} catch(IOException e) {
 			Assume.assumeTrue(false);
 		}
-		return false;
+		return null;
+	}
+
+	private boolean result(boolean success) {
+		return success;
+	}
+
+	private TextFragment newNode(TextFragment parent, String text, int type, String parameter) {
+		TextFragment node;
+		if(parent == null) {
+			node = new TextFragment(type, parameter);
+		} else {
+			node = parent.appendChild(new TextFragment(type, parameter));
+		}
+		if(text != null) {
+			TextFragment textNode;
+			textNode = new TextFragment(TextFragment.PLAIN_TYPE);
+			node.appendChild(textNode);
+			textNode.setContent(text);
+		}
+		return node;
+	}
+	private TextFragment newNode(TextFragment parent, int type, String parameter) {
+		return newNode(parent, null, type, parameter);
+	}
+	private TextFragment newNode(TextFragment parent, int type) {
+		return newNode(parent, null, type, null);
+	}
+	private TextFragment newNode(TextFragment parent, String text, int type) {
+		return newNode(parent, text, type, null);
+	}
+	private void addText(TextFragment parent, String text) {
+		TextFragment textNode;
+
+		if(parent == null) {
+			throw new Error("Parent must be set");
+		}
+
+		textNode = new TextFragment(TextFragment.PLAIN_TYPE);
+		parent.appendChild(textNode);
+		textNode.setContent(text);
+	}
+
+	//@Test
+	//public void emptyDocument() {
+	//	Assert.assertNotNull(parse(""));
+	//}
+
+	@Test
+	public void header() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "test", TextFragment.HEADER_TYPE);
+
+		fragment = parse("==test==\n");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void headerWithSpace() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "test", TextFragment.HEADER_TYPE);
+
+		fragment = parse("== test == \n");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void headerAndParagraph() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "test", TextFragment.HEADER_TYPE);
+		newNode(wanted, "hello", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("== test == \nhello");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void headerAndtwoParagraphs() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "hello", TextFragment.HEADER_TYPE);
+		newNode(wanted, "test hello", TextFragment.PARAGRAPH_TYPE);
+		newNode(wanted, "cool lol", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("== hello ==\ntest\nhello\n\ncool\nlol");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void headerWithEmAndParagraph() {
+		TextFragment wanted, header;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		header = newNode(wanted, TextFragment.HEADER_TYPE);
+		newNode(wanted, "hello", TextFragment.PARAGRAPH_TYPE);
+
+		addText(header, "t");
+		newNode(header, "est", TextFragment.STRONG_TYPE);
+		addText(header, "er");
+
+		fragment = parse("== t'''est'''er == \nhello");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void headerAndTemplate() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "Hello", TextFragment.PARAGRAPH_TYPE);
+		newNode(wanted, "Test", TextFragment.HEADER_TYPE);
+		newNode(wanted, "en-noun", TextFragment.TEMPLATE_TYPE);
+
+		fragment = parse("Hello\n\n====Test====\n{{en-noun}}\n");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void horizontalLineAndHeader() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+		newNode(wanted, "test", TextFragment.HEADER_TYPE);
+
+		fragment = parse("----\n== test ==");
+		assertTrue(fragment.equals(wanted));
 	}
 
 	@Test
-	public void emptyDocument() {
-		Assert.assertFalse(parse(""));
+	public void whitespaceAtStart() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "test ");
+		newNode(paragraph, "cool", TextFragment.STRONG_TYPE);
+
+		fragment = parse(" test ''' cool'''");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void whitespaceAtEnd() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "test ");
+		newNode(paragraph, "cool", TextFragment.STRONG_TYPE);
+
+		fragment = parse("test ''' cool''' ");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void whitespaceBefore() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "test ");
+		newNode(paragraph, "cool", TextFragment.STRONG_TYPE);
+
+		fragment = parse("test ''' cool'''");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void whitespaceAfter() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		newNode(paragraph, "cool ", TextFragment.STRONG_TYPE);
+		addText(paragraph, "test");
+
+		fragment = parse("'''cool ''' test");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void whitespaceMiddle() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		newNode(paragraph, "cool ", TextFragment.STRONG_TYPE);
+		newNode(paragraph, "test", TextFragment.EM_TYPE);
+
+		fragment = parse("'''cool ''' '' test''");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void mergeTextStyleMarkups() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		/* verify */
+		newNode(paragraph, "cool ", TextFragment.STRONG_TYPE);
+		newNode(paragraph, "test", TextFragment.EM_TYPE);
+
+		fragment = parse("'''cool ''''' test''");
+		assertTrue(fragment.equals(wanted));
 	}
 
 	@Test
-	public void justText() {
-		Assert.assertTrue(parse("hello\nhe"));
+	public void quoteAtStartAtTextStyleMarkup() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		newNode(paragraph, "'cool", TextFragment.STRONG_TYPE);
+
+		fragment = parse("''' 'cool '''");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void oneExtraQuoteAtTextStyleMarkup() {
+		TextFragment wanted, paragraph, em;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "'");
+		em = newNode(paragraph, TextFragment.EM_TYPE);
+		newNode(em, "cool'", TextFragment.STRONG_TYPE);
+		addText(paragraph, "a");
+
+		fragment = parse("''''''cool''''''a");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void threeExtraQuoteAtTextStyleMarkup() {
+		TextFragment wanted, paragraph, em;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "'''");
+		em = newNode(paragraph, TextFragment.EM_TYPE);
+		newNode(em, "cool'''", TextFragment.STRONG_TYPE);
+		addText(paragraph, "a");
+
+		fragment = parse("''''''''cool''''''''a");
+		assertTrue(fragment.equals(wanted));
 	}
 
 	@Test
-	public void headerAndText() {
-		parse("==hello==\nhe");
+	public void unendingTextStyle() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "This is ");
+		newNode(paragraph, "so cool", TextFragment.STRONG_TYPE);
+
+		fragment = parse("This is '''so cool");
+		assertTrue(fragment.equals(wanted));
 	}
 
 	@Test
-	public void headerAndTextWithSpaces() {
-		parse("== hello ==\nhe");
+	public void multiLineTemplate() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "This is ");
+		newNode(paragraph, "so cool", TextFragment.TEMPLATE_TYPE, "smallcaps");
+
+		fragment = parse("This is {{smallcaps\n|so cool\n\n}}");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void multiLineTemplate2() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "This is ");
+		newNode(paragraph, "so cool", TextFragment.TEMPLATE_TYPE, "smallcaps");
+
+		fragment = parse("This is {{smallcaps\n|\nso cool\n\n}}");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void multiLineTemplate3() {
+		TextFragment wanted, paragraph, parentTemplate;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, "hello", TextFragment.PARAGRAPH_TYPE);
+		paragraph = newNode(wanted, "Derived terms", TextFragment.HEADER_TYPE);
+
+		addText(paragraph, "This is ");
+		parentTemplate = newNode(wanted, TextFragment.TEMPLATE_TYPE, "der3");
+		newNode(parentTemplate, TextFragment.TEMPLATE_TYPE, "l||catckin");
+		newNode(parentTemplate, TextFragment.TEMPLATE_TYPE, "l||heat (in a cat)");
+		newNode(parentTemplate, TextFragment.TEMPLATE_TYPE, "l||pine marten");
+
+		fragment = parse("hello\n\n====Derived terms====\n{{der3\n" +
+			"|{{l||catkin}}\n" +
+			"|{{l||heat (in a cat)}}\n" +
+			"|{{l||pine marten}}\n" +
+			"}}");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void invalidMultiLineTemplate() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		newNode(wanted, "This is {{smallcaps a|so cool", TextFragment.PARAGRAPH_TYPE);
+		newNode(wanted, "}}", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("This is {{smallcaps\na|so cool\n\n}}");
+		assertTrue(fragment.equals(wanted));
 	}
 
 	@Test
-	public void headerWithOneEqualSignAtStart() {
-		parse("=== hello ==\nhe");
-	}
+	public void templateMarkup() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
 
-	@Test
-	public void headerWithOneEqualSignAtEnd() {
-		parse("== hello ===\nhe");
-	}
+		addText(paragraph, "hello ");
+		newNode(paragraph, "world", TextFragment.TEMPLATE_TYPE, "smallcaps");
+		addText(paragraph, "!");
 
-	@Test
-	public void headerWithNonEndingTextStyleMarkup() {
-		parse("== he'''llo ==\nhe");
+		fragment = parse("hello {{smallcaps|world}}!");
+		assertTrue(result(fragment.equals(wanted));
 	}
-
 	@Test
-	public void linkAndTemplateMerged() {
-		parse("te[[st {{smallcaps| thin]]g}}.");
+	public void linkMarkup() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "hello ");
+		newNode(paragraph, "url", TextFragment.LINK_TYPE);
+		addText(paragraph, "!");
+
+		fragment = parse("hello [[url]]!");
+		assertTrue(fragment.equals(wanted));
 	}
-
 	@Test
-	public void linkWithNonEndingBold() {
-		parse("[[li'''nk]].");
+	public void linkMarkupWithName() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "hello ");
+		newNode(paragraph, "url", TextFragment.LINK_TYPE, "world");
+		addText(paragraph, "!");
+
+		fragment = parse("hello [[url|world]]!");
+		assertTrue(fragment.equals(wanted));
 	}
-
 	@Test
-	public void linkWithBold() {
-		parse("he'''llo, [[link]] this.");
+	public void linkMarkupToOutside() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "hello ");
+		newNode(paragraph, "url", TextFragment.LINK_TYPE, "world");
+		addText(paragraph, "!");
+
+		fragment = parse("hello [url|world]!");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void templateWithPartialStrongMarkup() {
+		TextFragment wanted, paragraph, template;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+		addText(paragraph, "helloha");
+
+		template = newNode(paragraph, TextFragment.TEMPLATE_TYPE, "smallcaps");
+		addText(template, "uh");
+		newNode(template, "gr", TextFragment.STRONG_TYPE);
+
+		addText(paragraph, "eugh");
+
+		fragment = parse("helloha{{smallcaps|uh'''gr}}eugh");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void templateAndLinkInterleaved() {
+		fragment = parse("hello[[ha{{smallcaps|uh]]gr}}eugh");
+	}
+	@Test
+	public void linkAndTemplateInterleaved() {
+		TextFragment wanted;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		newNode(wanted, "hello{{smallcaps|gh[[uh}}gr]]eugh", TextFragment.PARAGRAPH_TYPE);
+
+		fragment = parse("hello{{smallcaps|gh[[uh}}gr]]eugh");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void linkWithExtraBracket() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+		addText(paragraph, "hello ");
+		newNode(paragraph, "world", TextFragment.LINK_TYPE);
+		addText(paragraph, "] cool");
+
+		fragment = parse("hello [world]] cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void multpleValidLinksAndTemplates() {
+		TextFragment wanted, paragraph;
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		paragraph = newNode(wanted, TextFragment.PARAGRAPH_TYPE);
+
+		addText(paragraph, "hello ");
+		newNode(paragraph, "this", TextFragment.LINK_TYPE);
+		addText(paragraph, " is ");
+		addText(paragraph, " complicated wikitext, ");
+		newNode(paragraph, "url", TextFragment.LINK_TYPE, "which");
+		addText(paragraph, " contains ");
+		newNode(paragraph, "lots", TextFragment.TEMPLATE_TYPE, "smallcaps");
+		addText(paragraph, " of ");
+		newNode(paragraph, "stuff" + ((char)0) + "gerugh", TextFragment.TEMPLATE_TYPE, "smallcaps");
+
+		fragment = parse("hello [[this]] is [[pretty]] complicated wikitext, [[url|which]] contains {{smallcaps|lots}} of {{smallcaps|stuff|gerugh}}\n");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listMarkupWithoutSpace() {
+		TextFragment wanted, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		item = newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("# test\n#* hello\n#* cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void doubleUnorderedListMarkupWithoutSpace() {
+		TextFragment wanted, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ul");
+		item = newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("* test\n** hello\n** cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listWithTwoLevels() {
+		TextFragment wanted, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+		item = newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("# test\n# hello\n# cool\n# * test\n# * cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listWithNoOwnItems() {
+		TextFragment wanted, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		item = newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		item = newNode(list, TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("# test\n# hello\n# * * test\n# * * cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listWithSublistAtMiddle() {
+		TextFragment wanted, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		item = newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("# test\n# hello\n# * test\n# cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listTypeChangeAtMiddle() {
+		TextFragment wanted, list;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "howdy", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("* howdy\n* hello\n# test\n# cool");
+		assertTrue(fragment.equals(wanted));
+	}
+	@Test
+	public void listTypeChangeAtMiddleInsideList() {
+		TextFragment wanted, mainList, list, item;
+
+		wanted = newNode(null, TextFragment.ROOT_TYPE);
+		mainList = newNode(wanted, TextFragment.LIST_TYPE, "ol");
+		item = newNode(mainList, "test", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ul");
+		newNode(list, "howdy", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "hello", TextFragment.LIST_ITEM_TYPE);
+
+		list = newNode(item, TextFragment.LIST_TYPE, "ol");
+		newNode(list, "test", TextFragment.LIST_ITEM_TYPE);
+		newNode(list, "cool", TextFragment.LIST_ITEM_TYPE);
+
+		fragment = parse("# test\n# * howdy\n# * hello\n# # test\n# # cool");
+		assertTrue(fragment.equals(wanted));
 	}
 }
